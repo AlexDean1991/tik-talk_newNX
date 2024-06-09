2e3b7d60d78f33fb889e64ea286cc2bb4b73d252
Ivan Chernyakov <3699818@mail.ru>
2024-06-09 19:52:40 +0300

    Add chats

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,10 +1,11 @@
 import {AsyncPipe, NgForOf} from '@angular/common';
-import {Component, inject} from '@angular/core';
+import {Component, computed, inject, signal} from '@angular/core';
 import {toObservable} from '@angular/core/rxjs-interop';
-import {ActivatedRoute, RouterLink} from '@angular/router';
-import {switchMap} from 'rxjs';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
+import {firstValueFrom, switchMap} from 'rxjs';
 import {ProfileHeaderComponent} from '../../common-ui/profile-header/profile-header.component';
 import {SvgIconComponent} from '../../common-ui/svg-icon/svg-icon.component';
+import {ChatsService} from '../../data/services/chats.sertvice';
 import {ProfileService} from '../../data/services/profile.service';
 import {ImgUrlPipe} from '../../helpers/pipes/img-url.pipe';
 import {PostFeedComponent} from './post-feed/post-feed.component';
@@ -26,19 +27,30 @@ import {PostFeedComponent} from './post-feed/post-feed.component';
 })
 export class ProfilePageComponent {
   profileService = inject(ProfileService)
+  chatsService = inject(ChatsService)
   route = inject(ActivatedRoute)
+  router = inject(Router)
 
   me$ = toObservable(this.profileService.me)
   subcribers$ = this.profileService.getSubscribersShortList(5)
 
+  isMyPage = signal(false)
 
   profile$ = this.route.params
     .pipe(
       switchMap(({id}) => {
+        this.isMyPage.set(id === 'me' || id === this.profileService.me()?.id)
         if (id === 'me') return this.me$
 
         return this.profileService.getAccount(id)
       })
     )
+
+  async sendMessage(userId: number) {
+    firstValueFrom(this.chatsService.createChat(userId))
+      .then((res) => {
+        this.router.navigate(['/chats', res.id])
+      })
+  }
 
 }
