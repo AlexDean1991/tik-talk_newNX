64ef2f9775e4d5215be7ae9bf415863f59aa382d
Ivan Chernyakov <3699818@mail.ru>
2024-06-09 16:43:42 +0300

    Add profile feed

--- src/app/pages/profile-page/post-feed/post-feed.component.ts
+++ src/app/pages/profile-page/post-feed/post-feed.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, HostListener, inject, Renderer2} from '@angular/core';
+import {firstValueFrom, fromEvent} from 'rxjs';
+import {PostService} from '../../../data/services/post.service';
 import {PostInputComponent} from '../post-input/post-input.component';
 import {PostComponent} from '../post/post.component';
 
@@ -13,5 +15,36 @@ import {PostComponent} from '../post/post.component';
   styleUrl: './post-feed.component.scss'
 })
 export class PostFeedComponent {
+  postService = inject(PostService)
+  hostElement = inject(ElementRef)
+  r2 = inject(Renderer2)
 
+  feed = this.postService.posts
+
+  @HostListener('window:resize')
+  onWindowResize() {
+    this.resizeFeed()
+  }
+
+
+  constructor() {
+    firstValueFrom(this.postService.fetchPosts())
+  }
+
+  ngAfterViewInit() {
+    this.resizeFeed()
+
+    fromEvent(window, 'resize')
+      .subscribe(() => {
+        console.log(12313)
+      })
+  }
+
+  resizeFeed() {
+    const {top} = this.hostElement.nativeElement.getBoundingClientRect();
+
+    const height = window.innerHeight - top - 24 - 24
+
+    this.r2.setStyle(this.hostElement.nativeElement, 'height', `${height}px`)
+  }
 }
